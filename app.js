2d3bf72925371469828748233ecd9da857463d01
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-19 17:02:40 +0500

    Add oauth

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require("passport-local-mongoose");
 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
 app.use(express.static('public'));
@@ -42,6 +44,19 @@ const User =  new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 //get
 app.get('/', (req, res) => {
